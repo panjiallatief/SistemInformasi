0f5d927e257c0a1b6618314b1a9f8a20c81ff2b0
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2023-08-08 14:37:02 +0700

    asd

---
+++ src/main/java/com/SistemInformasi/SistemInformasi/Controller/Controller.java
@@ -0,0 +1,13 @@
+package com.SistemInformasi.SistemInformasi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "/")
+    public String index() {
+        
+        return "index";
+    }  
+}
