da59694ead28eea21cd68501dda518329bca04d2
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2023-08-08 14:49:12 +0700

    asd

--- src/main/java/com/SistemInformasi/SistemInformasi/Repository/Sistem_Informasi_Repository.java
+++ src/main/java/com/SistemInformasi/SistemInformasi/Repository/Sistem_Informasi_Repository.java
@@ -1,7 +1,5 @@
 package com.SistemInformasi.SistemInformasi.Repository;
 
-import javax.print.attribute.standard.Media;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,9 +7,9 @@ import com.SistemInformasi.SistemInformasi.Entity.SistemInformasi;
 
 public interface Sistem_Informasi_Repository extends JpaRepository<SistemInformasi, Integer> {
     
-    @Query(value = "SELECT * FROM sistem_informasi WHERE namafile = ?1",
-        countQuery = "SELECT count(*) FROM sistem_informasi WHERE namafile = ?1",
-        nativeQuery = true)
-    Media getfilename(String namafile);
+    // @Query(value = "SELECT * FROM sistem_informasi WHERE namafile = ?1",
+    //     countQuery = "SELECT count(*) FROM sistem_informasi WHERE namafile = ?1",
+    //     nativeQuery = true)
+    // SistemInformasi getfilename(String namafile);
 
 }
