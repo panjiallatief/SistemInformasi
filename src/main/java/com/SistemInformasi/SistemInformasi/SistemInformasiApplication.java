174462c71f24a817b9a7fb0bf5c29ed72ad5fc8c
Panji Maulana Al Latief <65167200+panjiallatief@users.noreply.github.com>
2023-08-08 14:28:19 +0700

    Initial commit

---
+++ src/main/java/com/SistemInformasi/SistemInformasi/SistemInformasiApplication.java
@@ -0,0 +1,13 @@
+package com.SistemInformasi.SistemInformasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiApplication.class, args);
+	}
+
+}
