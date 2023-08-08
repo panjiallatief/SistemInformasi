aadd8342f84a7ac314d8fbe52e0d3c810283af03
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2023-08-08 14:47:13 +0700

    da

---
+++ src/main/java/com/SistemInformasi/SistemInformasi/Entity/SistemInformasi.java
@@ -0,0 +1,47 @@
+package com.SistemInformasi.SistemInformasi.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Sistem_Informasi")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SistemInformasi {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nama")  
+    private String nama;
+
+    @Column(name = "deskripsi", columnDefinition = "TEXT")
+    private String deskripsi;
+
+    @Column(name = "filename")  
+    private String filename;
+
+    @Column(name = "kategori")  
+    private String kategori;
+
+}
